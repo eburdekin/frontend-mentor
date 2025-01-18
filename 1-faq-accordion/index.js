7d578f30713b457e7eb45176daeab04bf213f853
Eileen Burdekin <eileenburdekin@proton.me>
2025-01-17 16:14:26 -0800

    faq accordion - add more css, update js

--- 1-faq-accordion/index.js
+++ 1-faq-accordion/index.js
@@ -4,11 +4,21 @@ questions.forEach((question) => {
   question.addEventListener("click", () => {
     const allAnswers = document.querySelectorAll(".faq-answer");
     const currentAnswer = question.nextElementSibling;
-    allAnswers.forEach((answer) => {
-      if (answer !== currentAnswer) {
-        answer.style.display = "none";
-      }
-    });
-    currentAnswer.style.display = "block";
+    const allQuestionIcons = document.querySelectorAll(".faq-question img");
+    const currentQuestionIcon = question.querySelector("img");
+    if (currentAnswer.style.display === "block") {
+      currentAnswer.style.display = "none";
+      currentQuestionIcon.src = "./assets/images/icon-plus.svg";
+      currentQuestionIcon.alt = "plus icon";
+    } else {
+      allAnswers.forEach((answer) => (answer.style.display = "none"));
+      allQuestionIcons.forEach((icon) => {
+        icon.src = "./assets/images/icon-plus.svg";
+        icon.alt = "plus icon";
+      });
+      currentAnswer.style.display = "block";
+      currentQuestionIcon.src = "./assets/images/icon-minus.svg";
+      currentQuestionIcon.alt = "minus icon";
+    }
   });
 });
